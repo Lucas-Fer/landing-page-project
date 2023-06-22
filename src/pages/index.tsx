56bd15dfc7c492a6a725a702ec019d255ebb83d5
lucas-fer <alikacildes@gmail.com>
2023-06-22 20:41:35 -0300

    style: config global css

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Título usando Plus Jakarta Sans</h1>
+      <p>Parágrafo usando Plus Jakarta Sans.</p>
+    </div>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    fontFamily: 'Plus Jakarta Sans, sans-serif',
+  },
+
+  body: {
+    backgroundColor: 'blue',
+    color: 'white',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontWeight: 400,
+  }
+})
