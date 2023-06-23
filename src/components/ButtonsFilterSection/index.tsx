da89ef4c5fd774e81599a9a3c899e5dd3d718246
lucas-fer <alikacildes@gmail.com>
2023-06-23 13:18:11 -0300

    button section html

---
+++ src/components/ButtonsFilterSection/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import {
+  ButtonsSection,
+  ComponentSection,
+  CustomButton,
+  SelectSection,
+} from './styles'
+
+export default function ButtonsFilterSection() {
+  const [activeButton, setActiveButton] = useState<number>(0)
+
+  function handlerActiveButton(btnId: number): void {
+    setActiveButton(btnId)
+  }
+
+  return (
+    <ComponentSection>
+      <ButtonsSection>
+        <CustomButton
+          active={activeButton === 1}
+          onClick={() => handlerActiveButton(1)}
+        >
+          Agências
+        </CustomButton>
+        <CustomButton
+          active={activeButton === 2}
+          onClick={() => handlerActiveButton(2)}
+        >
+          Chatbot
+        </CustomButton>
+        <CustomButton
+          active={activeButton === 3}
+          onClick={() => handlerActiveButton(3)}
+        >
+          Marketing Digital
+        </CustomButton>
+        <CustomButton
+          active={activeButton === 4}
+          onClick={() => handlerActiveButton(4)}
+        >
+          Geração de Leads
+        </CustomButton>
+        <CustomButton
+          active={activeButton === 5}
+          onClick={() => handlerActiveButton(5)}
+        >
+          Mídia Paga
+        </CustomButton>
+      </ButtonsSection>
+
+      <SelectSection>
+        <span>Ordenar por</span>
+        <select name="" id="">
+          <option value="date">Data de Publicação</option>
+        </select>
+      </SelectSection>
+
+      <hr />
+    </ComponentSection>
+  )
+}
