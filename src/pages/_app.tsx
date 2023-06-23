593f2f593260d94d88b563b0edfb712be0bfa540
lucas-fer <alikacildes@gmail.com>
2023-06-23 11:04:06 -0300

    style: header applicatrion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,38 @@
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import { Container, CustomH1, Header, HeaderContent } from '@/styles/pages/app'
+
+import Image from 'next/image'
+import headerAsset from '../assets/asset-header.png'
+import leadsterLogo from '../assets/logo.png'
 
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <div>
+        <Image src={leadsterLogo} alt="logo-leadster" />
+      </div>
+      <Header>
+        <HeaderContent>
+          <button>WEBINARS EXCLUSIVOS</button>
+          <CustomH1 variant="firstElement">Menos Conversinha, </CustomH1>
+          <CustomH1 variant="secondElement">
+            Mais Conversão
+            <Image src={headerAsset} alt="header-asset" />
+          </CustomH1>
+          <hr />
+          <p>
+            Conheça as estratégias que <strong> mudaram o jogo</strong> e como
+            aplicá-las no seu negócio
+          </p>
+        </HeaderContent>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,88 @@
+import { styled } from '..'
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  minHeight: '100vh',
+
+  div: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '5rem',
+  },
+})
+
+export const Header = styled('header', {
+  backgroundColor: '$terciaryBlue',
+  width: '100%',
+})
+
+export const HeaderContent = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+
+  margin: '0 auto',
+  width: '60%',
+  padding: '1.5rem',
+  minHeight: 350,
+
+  button: {
+    width: 'fit-content',
+    alignSelf: 'center',
+
+    border: `2px solid`,
+    borderColor: '$primaryBlue',
+    borderRadius: '40px 40px 40px 10px',
+
+    padding: '5px 10px',
+    color: '$secondaryBlue',
+
+    fontSize: '$sm',
+    fontWeight: '$bold',
+
+    marginTop: '-2rem',
+  },
+
+  img: {
+    objectFit: 'cover',
+    position: 'absolute',
+    right: '-28px',
+    transform: 'rotate(10deg)',
+  },
+
+  hr: {
+    margin: '0.5rem auto',
+    width: '75%',
+  },
+
+  p: {
+    fontSize: '$sm',
+    textAlign: 'center',
+  },
+})
+
+export const CustomH1 = styled('h1', {
+  textAlign: 'center',
+
+  variants: {
+    variant: {
+      firstElement: {
+        fontWeight: '$medium',
+      },
+
+      secondElement: {
+        width: 'fit-content',
+        display: 'inline-block',
+        margin: '0 auto',
+
+        color: '$primaryBlue',
+        fontSize: '$6xl',
+        position: 'relative',
+      },
+    },
+  },
+})
