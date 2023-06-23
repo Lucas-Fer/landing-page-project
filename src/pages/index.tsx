60c1a80d35fdad52fb391941d359b249e53c6b5d
lucas-fer <alikacildes@gmail.com>
2023-06-23 11:14:50 -0300

    style: home body

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import { HomeContainer } from '@/styles/pages/home'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>
+    <HomeContainer>
       <h1>Título usando Plus Jakarta Sans</h1>
       <p>Parágrafo usando Plus Jakarta Sans.</p>
-    </div>
+    </HomeContainer>
   )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,11 @@
+import { styled } from '..'
+
+export const HomeContainer = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  border: '1px solid black',
+  width: '60%',
+  margin: '0 auto',
+  padding: '1rem',
+})
