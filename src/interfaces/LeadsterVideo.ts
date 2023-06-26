3790f5baec80da8e8809d5494fc118da0d5ce59a
lucas-fer <alikacildes@gmail.com>
2023-06-26 13:28:32 -0300

    feat: request ofr leadster videos repository

---
+++ src/interfaces/LeadsterVideo.ts
@@ -0,0 +1,8 @@
+export interface LeadsterVideo {
+  videoInfo: {
+    videoid: string
+    videoDescription: string
+    videoTitle: string
+    videoThumb: string
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,51 @@ import LeadsterInfo from '@/components/LeadsterInfo'
 import LeadsterGuides from '@/components/LeadsterGuides'
 import { HomeContainer } from '@/styles/pages/home'
 import React from 'react'
+import { GetStaticProps } from 'next'
+import { LeadsterVideo } from '@/interfaces/LeadsterVideo'
 
-export default function Home() {
+export default function Home({ videoInfo }: LeadsterVideo) {
   return (
     <>
       <HomeContainer>
         <ButtonsFilterSection />
         <hr />
-        <LeadsterGuides />
+        <LeadsterGuides videoInfo={videoInfo} />
         <hr />
       </HomeContainer>
       <LeadsterInfo />
       <Footer />
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/youtube')
+
+    const data = await response.json()
+
+    const videoInfo = await data.videos.map((videoData: any) => {
+      return {
+        videoid: videoData.id.videoId ? videoData.id.videoId : 'none',
+        videoDescription: videoData.snippet.description
+          ? videoData.snippet.description
+          : 'none',
+        videoTitle: videoData.snippet.title,
+        videoThumb: videoData.snippet.thumbnails.default,
+      }
+    })
+
+    return {
+      props: {
+        videoInfo,
+      },
+    }
+  } catch (error) {
+    return {
+      props: {
+        data: [],
+      },
+    }
+  }
 }
